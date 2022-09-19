9b9aea4b6e32228f78ad391a54883c7fc9087e14
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2022-09-19 12:14:51 +0530

    Update iteration.c

--- iteration.c
+++ iteration.c
@@ -2,7 +2,8 @@
 #include<math.h>
 
 #define   f(x)   cos(x)-3*x+1
-#define   g(x)   (1+cos(x))/3
+#define   O(x)   (1+cos(x))/3
+#define differentiate(x)  -sin(x)/3
 
 int main()
 {
@@ -11,18 +12,20 @@ int main()
 	 /* Inputs */
 	 printf("Enter initial guess: ");
 	 scanf("%f", &x0);
+	  if(fabs(differentiate(x0))<1)
+        printf("Function Form is Correct. Iteration Method can be applied\n");
+      else{
+        printf("Function Form is NOT Correct. Iteration Method CAN'T be applied\n");
+        return 0;
+    }
 	 printf("Enter tolerable error: ");
 	 scanf("%f", &e);
-	 /* Implementing Fixed Point Iteration */
 	 printf("\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");
-	 do
-	 {
-		  x1 = g(x0);
+	 do{
+		  x1 = O(x0);
 		  printf("%f\t%f\t%f\t%f\n", x0, f(x0), x1, f(x1));
 		  x0 = x1;
 	 }while( fabs(f(x1)) > e);
-	 
 	 printf("\nRoot is %f", x1);
-	
 	 return(0);
 }
